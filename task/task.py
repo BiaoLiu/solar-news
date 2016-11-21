03efe9f20a489af5d916bda26c9f200d21768eae
BiaoLiu <452381072@qq.com>
2016-11-21 10:18:21 +0800

    wallstreet

--- task/task.py
+++ task/task.py
@@ -1,7 +1,9 @@
 # coding:utf-8
 from celery import task
-# from task.celery import app
 
+
+# from task.celery import app
+from spider.spider import parser_page
 
 
 @task(name='相加的task')
@@ -12,3 +14,11 @@ def add(x, y):
 @task(name='相加的task2')
 def add2(x, y):
     return x + y
+
+
+@task(name='华尔街数据抓取')
+def spider_wall():
+    keywords = ['太阳能', '光伏', '晶硅', '多晶硅', '硅片']
+
+    for item in keywords:
+        parser_page(item,1)
